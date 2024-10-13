89115e04dfcd6d36035faed9ad91e550449aa62a
Sam Merham <sammerham@icloud.com>
2024-10-12 19:14:27 -0700

    no email

--- prisma/migrations/20241013010545_/migration.sql
+++ prisma/migrations/20241013010545_/migration.sql
@@ -1,27 +1,26 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "publicKey" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "PassKey" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "credentialID" TEXT NOT NULL,
     "publicKey" TEXT NOT NULL,
     "counter" INTEGER NOT NULL,
-    "transports" TEXT[],
     "deviceType" TEXT NOT NULL,
     "backedUp" BOOLEAN NOT NULL,
-    "userId" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
 
     CONSTRAINT "PassKey_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_publicKey_key" ON "User"("publicKey");
 
 -- AddForeignKey
 ALTER TABLE "PassKey" ADD CONSTRAINT "PassKey_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
